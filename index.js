8c6d3570422c3a9bed26623c578fe33c3088b827
Md Raihan Uddin <raihanuddin.cse8.bu@gmail.com>
2025-07-21 05:19:31 -0500

    modify and update all files

--- index.js
+++ index.js
@@ -22,7 +22,10 @@ app.use(cors());
 app.use(express.json());
 
 
- const serviceAccount = require('./firebase-admin-key.json');
+const decodedKey = Buffer.from(process.env.FB_SERVICE_KEY,'base64').toString('utf-8');
+
+
+ const serviceAccount = JSON.parse(decodedKey)
 
  admin.initializeApp({
      credential: admin.credential.cert(serviceAccount)
@@ -51,7 +54,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const db = client.db("bioDataDB");
         const bioDataCollection = db.collection("bioData");
@@ -941,8 +944,8 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
